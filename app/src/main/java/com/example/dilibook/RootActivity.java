cb91f4e00b86c9449d7fdbaaa1694c11155ef51f
Arthur <and.edo.cs@gmail.com>
2023-04-21 22:06:37 +0400

    samsung project

--- app/src/main/java/com/example/dilibook/RootActivity.java
+++ app/src/main/java/com/example/dilibook/RootActivity.java
@@ -2,7 +2,9 @@ package com.example.dilibook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RootActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class RootActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
     }
+//    public void goBack(View v){
+  //      Intent intent = new Intent(this, MainActivity.class);
+    //    startActivity(intent);
+
+   // }
 }
